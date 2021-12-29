7fc1b041104aa2d68f0aa2d4028ba68e44385ae3
donisius.wigie@gmail.com <donisius.wigie@gmail.com>
2021-12-28 19:38:03 -0500

    add prettier

--- proof-of-concept/index.js
+++ proof-of-concept/index.js
@@ -1,43 +1,52 @@
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
-const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
+const videoElement = document.getElementsByClassName("input_video")[0];
+const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const canvasCtx = canvasElement.getContext("2d");
+const landmarkContainer = document.getElementsByClassName(
+  "landmark-grid-container"
+)[0];
 const grid = new LandmarkGrid(landmarkContainer);
 
 const emptyCoord = { x: 0, y: 0, z: 0 };
 
-const getDistance = (coord1, coord2 = emptyCoord) => (
+const getDistance = (coord1, coord2 = emptyCoord) =>
   Math.sqrt(
     (coord1.x - coord2.x) ** 2 +
-    (coord1.y - coord2.y) ** 2 +
-    (coord1.z - coord2.z) ** 2
-  )
-);
+      (coord1.y - coord2.y) ** 2 +
+      (coord1.z - coord2.z) ** 2
+  );
 
-const dotProduct = (coord1, coord2) => coord1.x * coord2.x + coord1.y * coord2.y + coord1.z * coord2.z;
+const dotProduct = (coord1, coord2) =>
+  coord1.x * coord2.x + coord1.y * coord2.y + coord1.z * coord2.z;
 
-const radToDegree = (angle) => angle * 180 / Math.PI;
+const radToDegree = (angle) => (angle * 180) / Math.PI;
 
 const isLeaningForward = (coords) => {
   const rightShoulder = coords[12];
   const rightHip = coords[24];
 
-  const hipToShoulderVector = { x: rightShoulder.x - rightHip.x, y: rightShoulder.y - rightHip.y, z: rightShoulder.z - rightHip.z };
+  const hipToShoulderVector = {
+    x: rightShoulder.x - rightHip.x,
+    y: rightShoulder.y - rightHip.y,
+    z: rightShoulder.z - rightHip.z,
+  };
   const hipWithShoulderY = { ...rightHip, y: rightShoulder.y };
 
-  const angle = Math.acos(dotProduct(hipToShoulderVector, hipWithShoulderY) / (getDistance(hipToShoulderVector) * getDistance(hipWithShoulderY)));
-  console.log(radToDegree(angle))
+  const angle = Math.acos(
+    dotProduct(hipToShoulderVector, hipWithShoulderY) /
+      (getDistance(hipToShoulderVector) * getDistance(hipWithShoulderY))
+  );
+  console.log(radToDegree(angle));
   return radToDegree(angle) <= 115;
-}
+};
 
 const constraints = {
-  isLeaningForward
-}
+  isLeaningForward,
+};
 
-const messages = document.getElementById('messages');
+const messages = document.getElementById("messages");
 
 const onResults = (results) => {
-  messages.innerHTML = '';
+  messages.innerHTML = "";
 
   if (!results.poseLandmarks) {
     // grid.updateLandmarks([]);
@@ -46,41 +55,46 @@ const onResults = (results) => {
 
   if (constraints.isLeaningForward(results.poseLandmarks)) {
     //do something
-    messages.innerHTML = 'You are leaning forward too much';
-  };
+    messages.innerHTML = "You are leaning forward too much";
+  }
 
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
   // Only overwrite existing pixels.
-  canvasCtx.globalCompositeOperation = 'source-in';
+  canvasCtx.globalCompositeOperation = "source-in";
 
   // Only overwrite missing pixels.
-  canvasCtx.globalCompositeOperation = 'destination-atop';
+  canvasCtx.globalCompositeOperation = "destination-atop";
   canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-  canvasCtx.globalCompositeOperation = 'source-over';
-  drawConnectors(
-    canvasCtx, results.poseLandmarks,
-    POSE_CONNECTIONS,
-    {color: '#00FF00', lineWidth: 4}
+    results.image,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
   );
 
-  drawLandmarks(
-    canvasCtx,
-    results.poseLandmarks,
-    {color: '#FF0000', lineWidth: 2}
-  );
+  canvasCtx.globalCompositeOperation = "source-over";
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+    color: "#00FF00",
+    lineWidth: 4,
+  });
+
+  drawLandmarks(canvasCtx, results.poseLandmarks, {
+    color: "#FF0000",
+    lineWidth: 2,
+  });
 
   canvasCtx.restore();
 
   // grid.updateLandmarks(results.poseWorldLandmarks);
-}
+};
 
-const pose = new Pose({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-}});
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+  },
+});
 
 pose.setOptions({
   modelComplexity: 1,
@@ -88,17 +102,17 @@ pose.setOptions({
   enableSegmentation: true,
   smoothSegmentation: true,
   minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  minTrackingConfidence: 0.5,
 });
 
 pose.onResults(onResults);
 
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await pose.send({image: videoElement});
+    await pose.send({ image: videoElement });
   },
   width: 1280,
-  height: 720
+  height: 720,
 });
 
 camera.start();
